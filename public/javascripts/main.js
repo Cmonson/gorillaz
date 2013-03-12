69181c17ef61f09346fc10075ea4692e78704cbc
david viramontes <dviramontes@gmail.com>
2013-03-11 21:14:52 -0600

    fixes mouseDebug() and adds dat.gui.js library

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,31 +1,45 @@
-// this site : https://dl.dropbox.com/u/24733418/serve/html/world.html
-// this code : https://dl.dropbox.com/u/24733418/serve/html/main.js
+/* dropbox version:
+	this site : https://dl.dropbox.com/u/24733418/serve/html/world.html
+	this code : https://dl.dropbox.com/u/24733418/serve/html/main.js
+*/
 
-// creates a world with a gravity of 10 m/s/s and sleeping allowed
-// gravity is +10 because of the coordinate system of the world 
-// where y & x axis go down 
+/*	
+	Main.js depends Box2DWeb.js and Processing.js
+	Author : @dreamPilot_
+    ---------------------------------------------
+	Note:
+	Box2DWeb uses Standard Units, meters, kilos, 
+	secs as well as radians for angles.
 
-// Box2D uses Standard Units, meters, kilos, 
-// secs as well as radians for angles.
+		 *
+	World specs:
+	Gravity: 10 m/s/s 
+	Sleeping :allowed
+	Coordinate system : same as processing.js
+
+	For most things a reference to p5 is really a reference 
+	to processing.js object.
+*/
 
 var world;
 var b2Vec2;
 var debugDraw;
 var body1;
-var objectCout; 
+var objectCout;
+var box;
+
+init(); 
 
 function init(){
 
-	// canvas + processing		
+	b2Vec2 = Box2D.Common.Math.b2Vec2
+	,	b2BodyDef = Box2D.Dynamics.b2BodyDef
+	,	b2Body = Box2D.Dynamics.b2Body
+	,	b2FixtureDef = Box2D.Dynamics.b2FixtureDef
+	,	b2World = Box2D.Dynamics.b2World
+	,	b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
+	,	b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
 
-	b2Vec2 = 	Box2D.Common.Math.b2Vec2
-			,	b2BodyDef = Box2D.Dynamics.b2BodyDef
-			,	b2Body = Box2D.Dynamics.b2Body
-			,	b2FixtureDef = Box2D.Dynamics.b2FixtureDef
-			,	b2World = Box2D.Dynamics.b2World
-			,	b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
-			,	b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
-		
 	debugDraw = new b2DebugDraw();
 		
 	function createGround(){
@@ -46,10 +60,10 @@ function init(){
 		createGround(world);
 		return world;
 	};
+
 	createWorld();
 };
 
-init();
 
 //  properties for this object with:
 var fixDef = new b2FixtureDef();
@@ -77,16 +91,36 @@ function createStaticBody(x,y){
 		holderDef.position.Set(x, y);
 
 	var holder = world.CreateBody(holderDef);
-		holder.CreateFixture(fixDef);
+	return holder.CreateFixture(fixDef);
 };
 
+// sketchProc accepts the processing function context 
 function sketchProc(p5){
 	// processing.js Directive 
 	/* @pjs globalKeyEvents="true"; */
 	
 	var framerate = 30;
 	var object;
+
+/*	SETUP & DRAW Functions
+	
+	TODO: Render box2d and processing
+	on the same canvas or maybe overlapping
+	canvases with opacity turned on.
+	Right now only one of them works.
+
+	Also, since processing and box2d use the 
+	same coordinate system we can probably 
+	call mouse and key tracking from processing 
+	and use it in the context of box2d
+    -------------------------------------------
+*/
 	with(p5){
+		
+		// TODO: As it stands,
+		// processing draw loop controls the framerate
+		// find another (more efficient) way to do this.
+
 		setup = function(){
 			size(550,300);
 			frameRate(framerate);
@@ -100,10 +134,10 @@ function sketchProc(p5){
 			world.CreateBody(object).CreateFixture(fixDef);
 			box = world.CreateBody(object);
 			box.CreateFixture(fixDef);
-			// console.log("width": p5.width);
 		};
+
 		draw = function() {
-			mouseDebug(false, this);
+			mouseDebug(true);
 			noFill();
 			world.SetDebugDraw(debugDraw);
 			update();
@@ -112,45 +146,68 @@ function sketchProc(p5){
 		};
 
 		keyPressed = function() {
+			
 			// report key - canvas focused
 			console.log("keycode: " + key.code);
+
 			if(keyCode == BACKSPACE ){
 				console.log("backspace was pressed");
 			}else if(key == 32){
 				console.log("/////   spacebar was pressed");
 				createDynamicBody(random(30),0);
-				
 			}else if(key == 10){
 				console.log("right");
-				pushRight();
+				pushRight(box);
 			}else if(key == 39){
 				console.log("left");
-				pushLeft();
+				pushLeft(box);
 			};
 		};
+	};
 
-		// *forces
+	/*
+		forces
+		------
+	*/
 
-		function pushRight(){	
-			box.ApplyForce(new b2Vec2(-200,0), box.GetWorldCenter());
-		};
+	function pushRight(obj){	
+		var force = new b2Vec2(-200,0);
+		obj.ApplyForce(force, obj.GetWorldCenter());
+	};
 
-		function update(){
-			world.Step(1 / framerate, 10, 10);
-			world.DrawDebugData();
-			// canvas.clearRect( 0 , 0 , canvas_width, canvas_height );
-			world.ClearForces();
-		};
+	/*
+		update functions
+		----------------
+	*/
+	
+	function update(){
+		world.Step(1 / framerate, 10, 10);
+		world.DrawDebugData();
 
-		function mouseDebug(bool, p5){
-			// convert this to meter ratio to match box2d
-			var mouseX = parseInt(mouseX % p5.width / 10);
-			var mouseY = parseInt(mouseY % p5.heigth / 10);
-			// debug - print to console
-			if(bool) console.log("mouseX: "+mouseX+"::"+"mouseY: "+mouseY);
-		};
+		// TODO: find out how to clear the canvas from boxes
+		// canvas.clearRect( 0 , 0 , p5.width, p5.height );
+		world.ClearForces();
+	};
+
+	/*
+		console debugging
+		-----------------
+	*/
+
+	function mouseDebug(bool){
+		// TODO: Convert this to meter ratio to match box2d
+		// probably something like this:
+		// var mouseX = parseInt(mouseX % p5.width / 10);
+		// var mouseY = parseInt(mouseY % p5.heigth / 10);
+
+		if(bool) console.log("mouseX: "+p5.mouseX+"::"+"mouseY: "+p5.mouseY);
 	};
 };
+
+/*
+	get canvas element and debug options for web2DWeb
+	-------------------------------------------------
+*/
 
 debugDraw.SetSprite( document.getElementById ("world_canvas").getContext("2d"));
 debugDraw.SetDrawScale(30.0);
@@ -158,6 +215,10 @@ debugDraw.SetFillAlpha(0.8);
 debugDraw.SetLineThickness(1.0);
 debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
 var canvas = document.getElementById('world_canvas');
+
+// Passes the sketchProcedure to the newly create 
+// Processing object. 
+// this techique comes from processingjs.org website
 var processing = new Processing(canvas, sketchProc);
 
 
